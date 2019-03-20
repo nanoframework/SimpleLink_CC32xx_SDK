b24adb2fddb62a8ef853a31582175ad74fbd2c23
José Simões <jose.simoes@eclo.solutions>
2019-03-20 16:59:34 +0000

    Initial commit
    - SDK 2.40.01.01

---
+++ ti/net/mqtt/package/package.defs.h
@@ -0,0 +1,13 @@
+/*
+ *  Do not modify this file; it is automatically 
+ *  generated and any modifications will be overwritten.
+ *
+ * @(#) xdc-F07
+ */
+
+#ifndef ti_net_mqtt__
+#define ti_net_mqtt__
+
+
+
+#endif /* ti_net_mqtt__ */ 
